86137bddd1d86868de01c311e93dbbcbf938fcb2
Maxime Albert <malbert002@emi.u-bordeaux.fr>
2021-04-02 21:20:43 +0200

    ajout de recieve

--- tftp.py
+++ tftp.py
@@ -26,11 +26,21 @@ def runServer(addr, timeout, thread):
     return s
 
 def send(addr_dest,  data, socket): #Pour envoyer
-    print("Requête get vers l'adresse de destination = ", addr)
+    print("Requête get vers l'adresse de destination = ", addr_dest)
     pass
 
 def recieve(addr_dest,  data, socket): #Pour recevoir
     print("Requête put vers l'adresse de destination = ", addr_dest)
+    socket.sendto(b'\x00\x04\x00\x00',addr_dest)
+    num_Paquet = 1
+    with open(filename, "w") as f:
+        while(True):
+            data, addr = socket.recvfrom(1500)
+            f.write(data)
+            socket.sendto(b'\x00\x04' + (x).to_bytes(4, 'big'), addr_dest)
+            if (len(data) != 512):
+                break
+            x += 1        
     pass
 
 ########################################################################
@@ -40,7 +50,7 @@ def recieve(addr_dest,  data, socket): #Pour recevoir
 
 def put(addr, filename, targetname, blksize, timeout):
     filename_byte = bytes(filename, 'utf-8')
-    data = (b'\x00') + (b'\x02') + filename_byte + (b'\x00') + (b'octets\x00')
+    data = b'\x00\x02' + filename_byte + b'\x00octets\x00'
     s_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s_client.bind(localhost, random.randint(50000,60000))
     s_client.sendto(data,addr)
@@ -51,7 +61,7 @@ def put(addr, filename, targetname, blksize, timeout):
 
 def get(addr, filename, targetname, blksize, timeout):
     filename_byte = bytes(filename, 'utf8')
-    data = (b'\x00') + (b'\x01') + filename_byte + (b'\x00') + b'octets\x00' 
+    data = b'\x00\x01' + filename_byte + b'\x00octets\x00' 
     print(data)                                                             
     s_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                     
     s_client.bind(localhost, random.randint(50000,60000))
