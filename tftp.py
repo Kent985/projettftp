892d24c77a77c625203aa64fef4d1f6635145b5e
Quentin Vallcaneras <quentin.vallcaneras@etu.u-bordeaux.fr>
2021-04-02 20:50:27 +0200

    hello there

--- tftp.py
+++ tftp.py
@@ -4,6 +4,7 @@ TFTP Module.
 
 import socket
 import sys
+import random
 
 
 ########################################################################
@@ -27,10 +28,14 @@ def runServer(addr, timeout, thread):
 
 def send(addr_dest,  data, socket): #Pour envoyer
     print("Requête get vers l'adresse de destination = ", addr)
+    for i in range(10):
+        socket.sendto(b'HELLO THERE', addr_dest)
     pass
 
 def recieve(addr_dest,  data, socket): #Pour recevoir
     print("Requête put vers l'adresse de destination = ", addr_dest)
+    for i in range(10):
+        socket.sendto(b'HELLO THERE', addr_dest)
     pass
 
 ########################################################################
@@ -42,8 +47,11 @@ def put(addr, filename, targetname, blksize, timeout):
     filename_byte = bytes(filename, 'utf-8')
     data = (b'\x00') + (b'\x02') + filename_byte + (b'\x00') + (b'octets\x00')
     s_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s_client.bind(localhost, random.randint(50000,60000))
+    s_client.bind(('localhost', random.randint(50000,60000)))
     s_client.sendto(data,addr)
+    while True:
+        data_serv, addr_serv = s_client.recvfrom(blksize)
+        print(data_serv.decode())
     pass
 
 ########################################################################
@@ -54,8 +62,11 @@ def get(addr, filename, targetname, blksize, timeout):
     data = (b'\x00') + (b'\x01') + filename_byte + (b'\x00') + b'octets\x00' 
     print(data)                                                             
     s_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                     
-    s_client.bind(localhost, random.randint(50000,60000))
-    s_client.sendto(data, addr)                                                    
+    s_client.bind(('localhost', random.randint(50000,60000)))
+    s_client.sendto(data, addr)
+    while True:
+        data_serv, addr_serv = s_client.recvfrom(blksize)
+        print(data_serv.decode())
     pass
 
 # EOF
