793510e339cc9f6ff45d61d0620908bfb8919ada
Quentin Vallcaneras <quentin.vallcaneras@etu.u-bordeaux.fr>
2021-04-25 02:45:29 +0200

    added threads

--- tftp-server.py
+++ tftp-server.py
@@ -8,6 +8,7 @@ import argparse
 import tftp
 import socket
 import random
+import threading
 
 TIMEOUT = 2
 PORT = 6969
@@ -18,13 +19,13 @@ parser.add_argument('-p', '--port', type=int, default=PORT)
 parser.add_argument('-t', '--timeout', type=int, default=TIMEOUT)
 parser.add_argument('-c', '--cwd',  type=str, default='')
 parser.add_argument('--thread', action='store_true')
-args = parser.parse_args()
+argscmd = parser.parse_args()
 
 # change current working directory
-if args.cwd != '': os.chdir(args.cwd)
+if argscmd.cwd != '': os.chdir(args.cwd)
 
 # run main server loop
-s1 = tftp.runServer((HOST, args.port), args.timeout, args.thread)
+s1 = tftp.runServer((HOST, argscmd.port), argscmd.timeout, argscmd.thread)
 
 while True:
     print("En attente de demande")
@@ -45,9 +46,21 @@ while True:
     print(args)
     print("\n")
     if opcode == 1:
-        tftp.send(addr, data, sTemp, filename)
+        if (argscmd.thread == True):
+            print("Threading activé")
+            t1 = threading.Thread(target = tftp.send, args=(addr,data,sTemp,filename))
+            t1.start()
+        else:
+            tftp.send(addr, data, sTemp, filename)
     elif opcode == 2:
-        tftp.recieve(addr, data, sTemp, filename)
+        if (argscmd.thread == True):
+            print("Threading activé")
+            t2 = threading.Thread(target = tftp.recieve, args = (addr,data,sTemp,filename))
+            t2.start()
+        else:
+            tftp.recieve(addr, data, sTemp, filename)
+        t1.join()
+        t2.join()
     else:
         sys.exit(1)
 
